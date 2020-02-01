3229197608c000ad8b133001e61a9fa4c1bfcf83
patrickmbell <patrickmbell@ufl.edu>
2020-02-01 02:03:02 -0500

    Added Ticketmaster script

---
+++ event_request.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+API_KEY = "WEVqY4AR8RjLNORXcmiYuG3A4RDL2Qpa"
+URL = "https://app.ticketmaster.com/discovery/v2/events.json?city={0}&stateCode={1}&apikey={2}&segmentName=Music"
+artists = {}
+
+def get_tm_artists(city, state):
+    REQUEST_URL = URL.format(city, state, API_KEY)
+    response = requests.get(url= REQUEST_URL)
+    json_data = json.loads(response.text)
+    return get_artist_list(json_data)
+
+def get_artist_list(json_data):
+    for elem in json_data["_embedded"]["events"]:
+       value = elem["name"]
+       if ':' in value:
+           value = value.split(':')[0]
+       if '-' in value:
+           value = value.split(' - ')[0]
+       if 'w/' in value:
+           value = value.split('w/')[0]
+       if '&' in value:
+           value = value.split('&')[0]
+       if '/' in value:
+           for string in value.split('/'):
+               artists[string] = 1
+       artists[value] = 1
+    artists_list  = [key for key in artists.keys()]
+    return artists_list
+
+print(get_tm_artists("Jacksonville", "FL"))
